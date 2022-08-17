e6a81e30212dc2047c036f7bb861f52bc2ddbcfb
kahoragachau <joelkahoragachau6@gmail.com>
2022-08-17 13:48:16 +0300

    - Done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// function multiply(){
+//     let num1 = 2;
+//     let num2 = 31;
+//     let multiply = num1 * num2;
+//     return multiply;
+// }
+let num1 = 31;
+let num2 = 2;
+let num3 = 4
+let num4 = 31
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()) + 1
+let mod = num3 % num4
+let max = Math.max(1,2,3,4,5,20)
+console.log(mod)
